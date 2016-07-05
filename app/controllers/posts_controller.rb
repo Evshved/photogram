5e9c2a3bb9948649b8b50bee3a9d577d684ed0c0
Shved <snuggleshved@gmail.com>
2016-07-05 20:36:51 +0300

    Add edit of post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,15 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def delete
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+  end
 private
   def post_params
     params.require(:post).permit(:image,:caption)
