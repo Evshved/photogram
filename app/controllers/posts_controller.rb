4342130b5462b152dd6bc277cdc78d49c84e7727
Shved <snuggleshved@gmail.com>
2016-07-06 18:52:30 +0300

    Add private method set_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @post = Post.all
   end
@@ -35,4 +37,9 @@ private
   def post_params
     params.require(:post).permit(:image,:caption)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
