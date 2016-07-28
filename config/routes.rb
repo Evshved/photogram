472b863c4d0755cf81aabd2a1398dba407d28f14
Shved <snuggleshved@gmail.com>
2016-07-28 22:48:41 +0300

    Add min and max length for user_name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root 'posts#index'
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
