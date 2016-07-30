b2c9494649395e7b21c6420ab842a74391defa2a
Shved <snuggleshved@gmail.com>
2016-07-31 02:51:14 +0300

    Add model Comment, configurate connections between controllers, update routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
   before_action :owned_post, only: [:edit, :update, :destroy]
+
   def index
     @post = Post.all
   end
