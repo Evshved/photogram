ca36bd67aee6a3c23252f0616e905b74d21d8ff2
Shved <snuggleshved@gmail.com>
2016-08-04 22:31:59 +0300

    Ready to use AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,10 @@ before_action :set_post
     @comment.user_id = current_user.id
 
     if @comment.save
-      flash[:success] = "You commented the hell out of that post!"
-      redirect_to :back
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
       flash[:alert] = "Check the comment form, something went horribly wrong."
       render root_path
