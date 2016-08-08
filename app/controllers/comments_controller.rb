57da2f773cf13092c5c8b6edc69e5b0961774fae
Shved <snuggleshved@gmail.com>
2016-08-08 23:33:18 +0300

    Add js scripts for create & delete comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,10 +18,13 @@ before_action :set_post
 
   def destroy
     @comment = @post.comments.find(params[:id])
-
-    @comment.destroy
-    flash[:success] = "Comment deleted :("
-    redirect_to root_path
+    if @comment.user_id == current_user.id
+      @comment.delete
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
   end
 
   private
